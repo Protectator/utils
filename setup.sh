94236cc7c1edbfc902b4372c52f3dc17d97471d9
Kewin Dousse <me@kewindousse.ch>
2024-04-05 01:10:45 +0200

    New setup file

---
+++ setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# This assumes we're on a fresh new install of a Debian/Ubuntu distro
+
+# Don't run if root
+if [ "$(id -u)" = "0" ]; then
+   echo "This script must not be run as root" 1>&2
+   exit 1
+fi
+
+# Needed pretty much everytime
+
+# homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# git
+sudo apt install git-all
+# - lazygit
+brew install jesseduffield/lazygit/lazygit
+# fzf
+brew install fzf
+# nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+nvm install --lts
+nvm use --lts
+# - tldr
+nvm i -g tldr
+# micro
+curl https://getmic.ro | bash
+# chezmoi
+brew install chezmoi
+
+# Useful to dev
+
+# jq
+sudo apt-get install jq
