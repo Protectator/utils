f8d2796a3fa4c1c181df958955d5c93d2711c699
Kewin Dousse <me@protectator.ch>
2024-08-25 17:37:46 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -73,10 +73,10 @@ install_zsh()
   echo "Downloading MesloLGS NF fonts in ~/fonts"
   mkdir -p fonts
   cd ~/fonts
-  wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
-  wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
-  wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
-  wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+  wget -q https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+  wget -q https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
+  wget -q https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
+  wget -q https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
   cd ~
   # zsh
   sudo apt -y install zsh
