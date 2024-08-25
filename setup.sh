cc9a55df41e9863eb681a8e1f96c74578961068a
Kewin Dousse <me@protectator.ch>
2024-08-25 17:58:10 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -31,7 +31,7 @@ echo "https://github.com/settings/ssh/new"
 cat ~/.ssh/id_ed25519.pub
 
 echo "Press any key to continue"
-# shellcheck disable=SCSC2162
+# shellcheck disable=SC2162
 read -s -n 1
 
 echo "Updating apt..."
