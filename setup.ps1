aac005ef205ccb4f30a8728dac461e8120973180
Kewin Dousse <me@kewindousse.ch>
2025-09-06 15:50:33 +0200

    Adding a PS install script

---
+++ setup.ps1
@@ -0,0 +1,21 @@
+Write-Host "Installing Chrome and Firefox..."
+winget install Google.Chrome
+winget install Mozilla.Firefox
+
+Write-Host "Installing VSCode ..."
+winget install Microsoft.VisualStudioCode
+
+Write-Host "Installing 7zip, VLC and PowerToys..."
+winget install 7zip.7zip
+winget install VideoLAN.VLC
+winget install Microsoft.PowerToys
+
+Write-Host "Installing Discord and Spotify..."
+winget install Discord.Discord
+winget install Spotify.Spotify
+
+Write-Host "Installing Git..."
+winget install Git.Git
+
+Write-Host "Done"
+pause
