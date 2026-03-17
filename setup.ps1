5219c41910c3c1407fd9682e798bcccc8b3ffe32
Kewin Dousse <me@kewindousse.ch>
2026-03-17 01:14:14 +0100

    Fixing PS script

--- setup.ps1
+++ setup.ps1
@@ -1,24 +1,24 @@
-Write-Host "Installing Chrome and Firefox..."
+Write-Output "Installing Chrome and Firefox..."
 winget install Google.Chrome
 winget install Mozilla.Firefox
 
-Write-Host "Installing VSCode ..."
+Write-Output "Installing VSCode ..."
 winget install Microsoft.VisualStudioCode
 
-Write-Host "Installing 7zip, VLC and PowerToys..."
+Write-Output "Installing 7zip, VLC and PowerToys..."
 winget install 7zip.7zip
 winget install VideoLAN.VLC
 winget install Microsoft.PowerToys
 
-Write-Host "Installing Discord and Spotify..."
+Write-Output "Installing Discord and Spotify..."
 winget install Discord.Discord
 winget install Spotify.Spotify
 
-Write-Host "Installing Git..."
+Write-Output "Installing Git..."
 winget install Git.Git
 
-Write-Host "Installing Winaero Tweaker..."
+Write-Output "Installing Winaero Tweaker..."
 winget install -e --id winaero.tweaker
 
-Write-Host "Done"
+Write-Output "Done"
 pause
